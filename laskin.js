cb57d7beb867548d9fc6611fff9e934b1b81edbe
JanitaA <59653594+JanitaA@users.noreply.github.com>
2020-12-13 14:25:17 +0200

    update

--- laskin.js
+++ laskin.js
@@ -1,48 +1,49 @@
 (function() {
-    // Convert sq mt to liters of paint
-    var litersOfPaint = 0;
-    var squareMeter = 0;
-    // Paste source here
-    var conversionRate = 12;
-  
-    function convertSquareMeterToLiters(squareMeter) {
-      // 12 sq mt to 1 liter of paint
-      litersOfPaint = squareMeter / conversionRate;
-      // Round number up to nearest whole number
-      litersOfPaint = Math.ceil(litersOfPaint);
-  
-      return litersOfPaint;
-    }
-  
-    // Find and update number of gallons element with returned number
-    function updateNumberOfLitersUI(numberOfLiters) {
-      // Find number of liters element
-      var numberOfLitersElement = document.querySelector("[data-ui='numberOfLiters']");
-      // Updating HTML with number of liters
-      numberOfLitersElement.innerHTML = numberOfLiters;
-    }
-  
-    // Find and update square feet element with number from input
-    function updateSquareMeterUI(numberOfSquareMeter) {
-      // Find square meter element
-      var squareMeterElement = document.querySelector("[data-ui='numberOfSquareMeter']");
-      // Updating square feet number element with actual number from input
-      squareMeterElement.innerHTML = numberOfSquareMeter;
-    }
-  
-    // Find square feet number
-    var squareMeterElement = document.querySelector("[data-ui='squareMeterNumber']");
-   
-    // Listen for input change
-    squareMeterElement.addEventListener("change", function(e) {
-      // Grabbing number of square feet from event object
-      var numberOfSquareMeter = e.target.value;
-      // Converting square feet to number of gallons
-      var numberOfLiters = convertSquareMeterToLiters(numberOfSquareMeter);
-  
-      // UPdating UI
-      updateNumberOfLitersUI(numberOfLiters);
-      updateSquareMeterUI(numberOfSquareMeter);
-    });
-  
-  })();
+  // All code here
+  // Convert sq ft to gallons of paint
+  var gallonsOfPaint = 0;
+  var squareFeet = 0;
+  // Paste source here
+  var conversionRate = 350;
+
+  function convertSquareFeetToGallons(squareFeet) {
+    // 350 sq ft to 1 gallon of paint
+    gallonsOfPaint = squareFeet / conversionRate;
+    // Round number up to nearest whole number
+    gallonsOfPaint = Math.ceil(gallonsOfPaint);
+
+    return gallonsOfPaint;
+  }
+
+  // Find and update number of gallons element with returned number
+  function updateNumberOfGallonsUI(numberOfGallons) {
+    // Find number of gallons element
+    var numberOfGallonsElement = document.querySelector("[data-ui='numberOfGallons']");
+    // Updating HTML with number of gallons
+    numberOfGallonsElement.innerHTML = numberOfGallons;
+  }
+
+  // Find and update square feet element with number from input
+  function updateSquareFeetUI(numberOfSquareFeet) {
+    // Find square feet element
+    var squareFeetElement = document.querySelector("[data-ui='numberOfSquareFeet']");
+    // Updating square feet number element with actual number from input
+    squareFeetElement.innerHTML = numberOfSquareFeet;
+  }
+
+  // Find square feet number
+  var squareFeetElement = document.querySelector("[data-ui='squareFeetNumber']");
+ 
+  // Listen for input change
+  squareFeetElement.addEventListener("change", function(e) {
+    // Grabbing number of square feet from event object
+    var numberOfSquareFeet = e.target.value;
+    // Converting square feet to number of gallons
+    var numberOfGallons = convertSquareFeetToGallons(numberOfSquareFeet);
+
+    // UPdating UI
+    updateNumberOfGallonsUI(numberOfGallons);
+    updateSquareFeetUI(numberOfSquareFeet);
+  });
+
+})();
