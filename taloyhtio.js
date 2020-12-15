b6024ab72424ca8c8daaf7a75403df1f7bf60e38
Väinö Rihti <vaino.rihti@edu.turkuamk.fi>
2020-12-15 12:41:51 +0200

    Update taloyhtio.js and css.js

--- taloyhtio.js
+++ taloyhtio.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "./index.css";
+const DottedBox = () => (
+  <div className="DottedBox">
+    <p className="DottedBox_content">Get started with CSS styling</p>
+  </div>
+);
 
+export default DottedBox;
 const Tilanperustiedot = ({ name, apartmentinfo, area}) => {
     return (
         <li>
