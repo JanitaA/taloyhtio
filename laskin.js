c95a3ddb5192dd3187c82b02e7f4d5c19e74ae2a
tilaiho19 <timo.laihonen@edu.turkuamk.fi>
2020-12-16 20:34:52 +0200

    Update laskin.js

--- laskin.js
+++ laskin.js
@@ -1,6 +1,8 @@
+
 (function() {
-var gallonsOfPaint = 0;
+  var gallonsOfPaint = 0;
   var squareFeet = 0;
+
   var conversionRate = 350;
 
   function convertSquareFeetToGallons(squareFeet) {
@@ -11,23 +13,50 @@ var gallonsOfPaint = 0;
   }
 
   function updateNumberOfGallonsUI(numberOfGallons) {
+ 
     var numberOfGallonsElement = document.querySelector("[data-ui='numberOfGallons']");
+   
     numberOfGallonsElement.innerHTML = numberOfGallons;
   }
 
   function updateSquareFeetUI(numberOfSquareFeet) {
-    var squareFeetElement = document.querySelector("[data-ui='numberOfSquareFeet']");
+    const squareFeetElement = document.getElementById('squareFeetElement');
+  
     squareFeetElement.innerHTML = numberOfSquareFeet;
   }
 
-  var squareFeetElement = document.querySelector("[data-ui='squareFeetNumber']");
- 
-  squareFeetElement.addEventListener("change", function(e) {
-    var numberOfSquareFeet = e.target.value;
-    var numberOfGallons = convertSquareFeetToGallons(numberOfSquareFeet);
 
+ 
+  document.getElementById("squareFeetElement").addEventListener( 'change', updateValue); 
+
+    
+    function updateValue(e){
+      var numberOfSquareFeet = e.target.value;
+      var numberOfGallons = convertSquareFeetToGallons(numberOfSquareFeet);
+    }
+
+  
+
+  
     updateNumberOfGallonsUI(numberOfGallons);
     updateSquareFeetUI(numberOfSquareFeet);
   });
 
-});
+
+
+    var apartments = React.createElement('section', {'id':'apartments'},
+    React.createElement('h2', null, 'Apartments',
+    
+    React.createElement("ul",null,
+    React.createElement("li", null, "2h + K" ),
+    React.createElement("li", null, "1h + kk"),
+    React.createElement("li", null, "3h + kk")
+    )
+    )
+    );
+    
+     ReactDOM.render(apartments, document.getElementById('react-root'));
+
+
+
+
